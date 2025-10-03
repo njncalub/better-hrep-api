4cae385abc3bcbdd951f7c4a9cdbc5840da2231b
Nap Joseph Calub <njncalub@gmail.com>
2025-10-04 03:58:14 +0800

    feat: remove mappings

--- main.ts
+++ main.ts
@@ -2,14 +2,12 @@ import { OpenAPIHono } from "@hono/zod-openapi";
 import { swaggerUI } from "@hono/swagger-ui";
 import { congressRouter } from "./routes/congress.ts";
 import { peopleRouter } from "./routes/people.ts";
-import { mappingsRouter } from "./routes/mappings.ts";
 
 const app = new OpenAPIHono({ strict: false });
 
 // Mount routes
 app.route("/", congressRouter);
 app.route("/", peopleRouter);
-app.route("/", mappingsRouter);
 
 // The OpenAPI documentation will be available at /doc
 app.doc("/doc", {
